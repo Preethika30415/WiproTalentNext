ed828dd70ca4f6cafa9e4c8b22ec31bb27bb4d4a
Preethika30415 <45070917+Preethika30415@users.noreply.github.com>
2020-07-13 23:12:32 +0530

    Add files via upload

---
+++ Oops and Inheritance/src/com/wipro/mettl/IdentifyPossibleWords.java
@@ -0,0 +1,34 @@
+package com.wipro.mettl.strings2;
+
+public class IdentifyPossibleWords 
+{
+	public String identifyPossibleWords(String input1,String input2)
+	{
+		String arr[]=input2.split(":");
+		String temp="",fin="";
+		int count=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			temp=arr[i];
+			count=0;
+			if(temp.length()==input1.length())
+			{
+				for(int j=0;j<temp.length();j++)
+				{
+					if(input1.charAt(j)!='_')
+					{
+						if(Character.toUpperCase(input1.charAt(j))==Character.toUpperCase(temp.charAt(j)))
+						{
+							count++;
+						}
+					}
+				}
+				if(count==temp.length()-1)
+					fin=fin+temp.toUpperCase()+":";
+			}
+		}
+		if(fin=="")
+			return "ERROR-009";
+		return fin.substring(0,fin.length()-1);
+	}
+}
